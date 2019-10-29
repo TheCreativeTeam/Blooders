3bc5e7b4399d678eee3d09be79e2fe090277251c
obada <obadaquraan@yahoo.com>
2019-10-29 12:29:14 +0200

    fix js files

--- js/app.js
+++ js/app.js
@@ -1,101 +1,53 @@
-'use strict'
-Donar.all=[];
+Donar.all = [];
 
-function Donar(name,bloodType,location,age,mail) {
+function Donar(name, bloodType, location, age, email) {
     this.name = name,
-    this.bloodType=bloodType,
-    this.location=location,
-    this.age=age,
-    this.mail=mail,
-    Donar.all.push(this)
+        this.bloodType = bloodType,
+        this.location = location,
+        this.age = age,
+        this.email = email,
+        Donar.all.push(this)
 }
 
-var donar1=new Donar('ahmad','O+','Amman',36,'ahmad_lkki@gmail.com')
-var form = document.getElementById("donor");
-console.log('form : ', form);
 
-// console.log('container : ', container);
-function addElement(tagName, container, text) {
-    var element = document.createElement(tagName);
-    container.appendChild(element);
-    if (text) {
-        element.textContent = text;
-      }
-    
-      return element;
-    }
+var donorsArray = []
+donorsArray = JSON.parse(localStorage.getItem('donorKey'))
 
 
-    
+for (i = 0; i < donorsArray.length; i++) {
+
+    var donorInfo = donorsArray[i];
+    new Donar(donorInfo.name, donorInfo.bloodType, donorInfo.location, donorInfo.age, donorInfo.email);
 
 
-
-
-        function submitHandler(event){
-            event.preventDefault();
-
-
-            var donorName=event.target.name.value;
-
-            var donorBlood=event.target.type.value;
-
-            var donorLocation=event.target.location.value;
-
-            var donorAge=event.target.age.value;
-            
-            
-            
-            if (donorAge<18){
-                alert('You are under 18')
-            }
-            else{
-                
-                var newDonor= new Donar(donorName,donorBlood,donorLocation,donorAge);
-                // Donar.all.push(newDonor);
-                
-                 localStorage.setItem('donorKey',JSON.stringify(Donar.all));
-                // renderToHospital ();
-                // returnDate();
-
-            }
-        }
-
-
-
-    
-
-    form.addEventListener('submit', submitHandler);
-
- 
-    
-    
-
-    function renderToHospital(){
-
-for(var i=0;i<Donar.all.length;i++){
-
-        var donarInstance=Donar.all[i];
-        var li = document.createElement('li');
-        
-        container.appendChild(li);
-        li.textContent=donarInstance.name+' '+donarInstance.bloodType+' '+donarInstance.location+' '+donarInstance.age;
 }
-    }
 
-// function returnDate(){    
-//  var donorsArray =JSON.parse(localStorage.getItem('donorKey'));
-//  if(donorsArray){
-// for(var i=0;i<donorsArray.length;i++){
 
-//     var instanceFromStorage=donorsArray[i];
-//     var instanceFromConsractor=Donar.all[i];
 
-//     // instanceFromConsractor.push(instanceFromStorage);
-//     instanceFromConsractor.name=instanceFromStorage.name;
-//     instanceFromConsractor.bloodType=instanceFromStorage.bloodType;
-//     instanceFromConsractor.location=instanceFromStorage.location;
-//     instanceFromConsractor.age=instanceFromStorage.age;
-//     instanceFromConsractor.mail=instanceFromStorage.mail;
-// }
-// }
-// }
+
+Hospital.all = [];
+function Hospital(name, location, type, quantity, email) {
+    this.name = name,
+        this.location = location,
+        this.type = type,
+        this.quantity = quantity,
+        this.email = email,
+        Hospital.all.push(this)
+}
+
+
+var demands = [];
+demands = JSON.parse(localStorage.getItem('hospitalKey'));
+
+
+for (var i = 0; i < demands.length; i++) {
+
+    var hospitalInfo = demands[i];
+    new Hospital(hospitalInfo.name, hospitalInfo.bloodType, hospitalInfo.location, hospitalInfo.age, hospitalInfo.email);
+
+
+
+
+
+}
+
--- js/hospital.js
+++ js/hospital.js
@@ -1,30 +1,23 @@
-function Hospital(name, location, type, quantity, email) {
-    this.name = name,
-        this.location = location,
-        this.type = type,
-        this.quantity = quantity,
-        this.email = email,
-        Hospital.all.push(this)
- }
- Hospital.all = [];
- var hospitalForm = document.getElementById('hospital');
+
+
+var hospitalForm = document.getElementById('hospital');
 
 
 
-    hospitalForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        var name = event.target.name.value;
-        var location = event.target.location.value;
-        var type = event.target.type.value;
-        var quantity = event.target.quantity.value;
-        var email = event.target.email.value;
-        var newHospital = new Hospital(name, location, type, quantity, email)
-        console.log(newHospital);
-        console.log('Hospital.all : ', Hospital.all);
-        localStorage.setItem('hospitalKey', JSON.stringify(Hospital.all));
-        // newHospital.render();
-    });
- 
+hospitalForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var name = event.target.name.value;
+    var location = event.target.location.value;
+    var type = event.target.type.value;
+    var quantity = event.target.quantity.value;
+    var email = event.target.email.value;
+    var newHospital = new Hospital(name, location, type, quantity, email)
+    console.log(newHospital);
+    // console.log('Hospital.all : ', Hospital.all);
+    localStorage.setItem('hospitalKey', JSON.stringify(Hospital.all));
+    // newHospital.render();
+});
+
 //  var waitingList = document.getElementById('list');
 
 
