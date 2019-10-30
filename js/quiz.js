cdaf919c144fccebb3ceb03a3b2a2b66197cbf4f
SajaSwalgah <saja.swalgah@yahoo.com>
2019-10-30 21:00:16 +0200

    playing with CSS

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,9 @@
+
+
+
+
+//I'm using the code from https://webdevtrick.com/create-javascript-quiz-program/
+
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
@@ -38,13 +44,14 @@ Question.prototype.isCorrectAnswer = function(choice) {
 var questions = [
     new Question("One of this list cannot donate:", ["An athletic", "A 22-years old adult","Cancer patient", "A  a person with 55-kg"], "Cancer patient"),
     new Question("How much blood an adult has?", ["4-5 liters", "10 liters", "1 liters", "450 ml"], "4-5 liters"),
-    new Question("From whom a patient with O+ blood type can get blood?", ["O- blood type person", "AB+ blood type person", "A+ blood type person", "B- blood type person"], "O- blood type person"),
+    new Question("A patient with O+ blood type can get blood from", ["O- blood type person", "AB+ blood type person", "A+ blood type person", "B- blood type person"], "O- blood type person"),
     new Question("Why you have to provide your personl info when donating?", ["For chatting", "To indruce your self", "NON", "For medical history"], "For medical history"),
-    new Question("For whom an AB- blood type person can give blood", ["A- blood type patient", "B+ blood type patient", "AB+ blood type patient", "All"], "All")
+    new Question("An AB- blood type person can give blood for", ["A- blood type patient", "B+ blood type patient", "AB+ blood type patient", "All"], "All"),
+    new Question("Who is the patient who will find it hard to find a donater according to the blood types", ["A- blood type patient", "B+ blood type patient", "AB+ blood type patient", "O- blood type patient"], "O- blood type patient")
 ];
 
  
-function populate() {
+function process() {
     if(quiz.isEnded()) {
         showScores();
     }
@@ -69,7 +76,7 @@ function guess(id, guess) {
     var button = document.getElementById(id);
     button.onclick = function() {
         quiz.guess(guess);
-        populate();
+        process();
     }
 };
  
@@ -81,8 +88,8 @@ function showProgress() {
 };
  
 function showScores() {
-    var gameOverHTML = "<h1>Result</h1>";
-    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var gameOverHTML = "<h1>Your Result</h1>";
+    gameOverHTML += "<h2 id='score'> You got: " + quiz.score + "</h2>";
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
 };
@@ -93,6 +100,6 @@ function showScores() {
 var quiz = new Quiz(questions);
  
 // display quiz
-populate();
+process();
 
 //https://webdevtrick.com/create-javascript-quiz-program/
